27fca966210c4e20c473d612be8e8263defb9c90
DHARANISH45 <dharanishslc@gmail.com>
2024-11-12 00:58:18 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFVZ2saCTDTw1JYChx0gBgSBORk2cDNlU",
+  authDomain: "instagram-clone-45.firebaseapp.com",
+  projectId: "instagram-clone-45",
+  storageBucket: "instagram-clone-45.firebasestorage.app",
+  messagingSenderId: "735684928442",
+  appId: "1:735684928442:web:de140f7562e5a6b63c7383"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
---
+++ src/timeline/Suggestions.js
@@ -0,0 +1,66 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Suggestions.css";
+
+function Suggestions() {
+  return (
+    <div className="suggestions">
+      <div className="suggestions__title">Suggestions for you</div>
+      <div className="suggestions__usernames">
+        <div className="suggestions__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>D</Avatar>
+            </span>
+            <div className="username__info">
+              <span className="username">dk_dhina</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="suggestions__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>D</Avatar>
+            </span>
+            <div className="username__info">
+              <span className="username">death-line</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="suggestions__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>S</Avatar>
+            </span>
+            <div className="username__info">
+              <span className="username">_sujan</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="suggestions__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>N</Avatar>
+            </span>
+            <div className="username__info">
+              <span className="username">Naveen@12</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Suggestions;
